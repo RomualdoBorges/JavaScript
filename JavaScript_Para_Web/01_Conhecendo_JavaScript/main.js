eca255baf547b77693539632ee37d7a0fdc34ea1
geomonitora@gmail.com <geomonitora@gmail.com>
2023-04-10 07:28:48 -0300

    Conhecendo o JavaScript

---
+++ JavaScript_Para_Web/01_Conhecendo_JavaScript/main.js
@@ -0,0 +1,7 @@
+// alert("Olá Mundo!"); - Função que cria uma janela com uma mensagem para o usuário.
+
+// querySelector(""); - Função capaz de buscar e trazer para nós o elemento que informarmos o seletor.
+
+// document - Palavra reservada que representa todo o documento HTML.
+
+document.querySelector(".tecla_pom");
